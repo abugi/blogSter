fa783fe36e18acedd8217d1cb7032d39ad380309
abugi <abugiboi@gmail.com>
2020-02-27 22:12:29 +0100

    minor code improvement

--- tests/header.test.js
+++ tests/header.test.js
@@ -32,32 +32,3 @@ test("Clicking login starts OAuth flow", async done => {
 
   done();
 });
-
-test.only("Show logout button when a user login", async done => {
-  const id = "5e543dd68c0b1d17dc1d0b02";
-
-  const Buffer = require("safe-buffer").Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    "base64"
-  );
-
-  const Keygrip = require("keygrip");
-  const keys = require("../config/keys");
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign("session =" + sessionString);
-  const verify = keygrip.verify("session =" + sessionObject, sig);
-
-  console.log(verify);
-
-  await page.setCookie({ name: "session", value: sessionString });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("http://localhost:3000");
-
-  done();
-});

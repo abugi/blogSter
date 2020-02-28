a83266158cc9c6865c1bef6085359136f3d92105
abugi <abugiboi@gmail.com>
2020-02-28 10:02:58 +0100

    bug fix

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,27 +1,27 @@
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
 
-// let browser, page;
+let browser, page;
 
-// beforeEach(async () => {
-//   browser = await puppeteer.launch({
-//     headless: true,
-//     args: ["--no-sandbox"]
-//   });
-//   page = await browser.newPage();
-//   await page.goto("http://localhost:3000");
-// });
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  });
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+});
 
-// afterEach(async () => {
-//   await browser.close();
-// });
+afterEach(async () => {
+  await browser.close();
+});
 
-// test("Header has the correct brand name", async done => {
-//   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+test("Header has the correct brand name", async done => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
-//   expect(text).toEqual("Blogster");
+  expect(text).toEqual("Blogster");
 
-//   done();
-// });
+  done();
+});
 
 // test("Clicking login starts the OAuth flow", async done => {
 //   await page.click(".right a");
